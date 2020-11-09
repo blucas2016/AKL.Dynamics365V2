190d115739e21d34afd7f09b7c58ad86716066e7
bruno lucas <bruno.lucas@hotmail.co.nz>
2020-11-09 23:06:01 +1300

    react dialog

---
+++ AKL.Dynamics365.WebResources/Webresources/js/AKL_D365_Invoke_REACT_SetUpJobInterview.js
@@ -0,0 +1,45 @@
+﻿var AKL;
+
+(function (AKL) {
+
+    var InterviewProcess = (function () {
+        function InterviewProcess() {
+        }
+
+        InterviewProcess.ScheduleInterview = function (formContext) {
+            debugger;
+
+            var data = {
+                text: formContext.data.entity.getPrimaryAttributeValue(),
+                date: formContext.getAttribute("createdon").getValue()
+            };
+
+            var dialogParameters = {
+                pageType: "webresource",
+                webresourceName: "bru_/AKL_REACT_SetUpJobInterview.html",
+                data: JSON.stringify(data)
+            };
+
+            var navigationOptions = {
+                target: 2,
+                width: 400,
+                height: 300,
+                position: 1
+            };
+
+            Xrm.Navigation.navigateTo(dialogParameters, navigationOptions).then(
+                function (returnValue) {
+                    console.log(returnValue);
+                    
+                },
+                function (e) {
+                    Xrm.Navigation.openErrorDialog(e);
+                });
+
+        };        
+
+        return InterviewProcess;
+    }());
+
+    AKL.InterviewProcess = InterviewProcess;
+})(AKL || (AKL = {}));
